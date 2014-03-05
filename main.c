86028ca680db4f9d3139f429fcb4be19c73da0e9
sla89 <skullsplitter@die-optimisten.net>
2014-03-05 10:13:21 +0100

    added file headers

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 /*
- * main.c
+ * Scheduler: main.c
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: 05.03.2014
+ * Description:
+ * TODO
  */
 int main(void) {
 	
--- scheduler.c
+++ scheduler.c
@@ -1,9 +1,9 @@
 /*
- * Scheduler: scheduler.h
+ * Scheduler: scheduler.c
  * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
  *
  * Created on: 05.03.2014
- * Description:
+ * Description: 
  * TODO
  */
 
