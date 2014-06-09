a9a06a2c3d57514c5d0821c4902ca2c8b9badb43
thomasgaida <thomas.gaida@students.fhv.at>
2014-06-09 10:23:52 +0200

    added swi led and gpio

--- swi/sc_swi.c
+++ swi/sc_swi.c
@@ -16,13 +16,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void SwiForward(SyscallArgData* data) {
+
+	uint32_t result = -1;
 
 	switch (data->swiNumber) {
 
-	case 0:
-		SwiStdioPrintf((const char*)data->arg1);
+	case SYSCALL_PRINTF:
+		SwiStdioPrintf((const char*) data->arg1);
+		break;
+	case SYSCALL_LED_ON_0:
+		SwiLed0On();
+		break;
+	case SYSCALL_LED_ON_1:
+		SwiLed1On();
+		break;
+	case SYSCALL_LED_ON_2:
+		SwiLed2On();
+		break;
+	case SYSCALL_LED_ON_3:
+		SwiLed3On();
+		break;
+	case SYSCALL_LED_OFF_0:
+		SwiLed0Off();
+		break;
+	case SYSCALL_LED_OFF_1:
+		SwiLed1Off();
+		break;
+	case SYSCALL_LED_OFF_2:
+		SwiLed2Off();
+		break;
+	case SYSCALL_LED_OFF_3:
+		SwiLed3Off();
+		break;
+	case SYSCALL_GPIO_DIR_SET:
+		SwiGPIODirModeSet((unsigned int) data->arg1, (unsigned int) data->arg2,
+				(unsigned int) data->arg3);
+		break;
+	case SYSCALL_GPIO_PIN_WRITE:
+		SwiGPIOPinWrite((unsigned int) data->arg1, (unsigned int) data->arg2,
+				(unsigned int) data->arg3);
+		break;
+	case SYSCALL_GPIO_PIN_READ:
+		result = SwiGPIOPinRead((unsigned int) data->arg1,
+				(unsigned int) data->arg2);
+		data->result = result;
+		break;
+	case SYSCALL_LED_INIT_LED_0:
+		SwiLedInit0();
+		break;
+	case SYSCALL_LED_INIT_LED_1:
+		SwiLedInit3();
+		break;
+	case SYSCALL_LED_INIT_LED_2:
+		SwiLedInit2();
+		break;
+	case SYSCALL_LED_INIT_LED_3:
+		SwiLedInit1();
+		break;
+	case SYSCALL_LED_INIT_REGISTER:
+		SwiLedInitRegister();
+		break;
+		case SYSCALL_LEDS_INIT:
+		SwiLedsInit();
 		break;
 
 	default:
@@ -35,17 +91,16 @@ void SwiForward(SyscallArgData* data) {
 //#pragma INTERRUPT(swi_handler, SWI)
 //interrupt void swi_handler(SyscallArgData* data) {
 
-void SwiHandler(SyscallArgData* data,Context* context) {
+void SwiHandler(SyscallArgData* data, Context* context) {
 
-	    printf("SwiHandler Swi Forward\n");
-	    SwiForward(data);
+	printf("SwiHandler Swi Forward\n");
+	SwiForward(data);
 
-	    Process* proc =  SchedulerCurrentProcess();
+	Process* proc = SchedulerCurrentProcess();
 
-	    if(proc != NULL && proc->state==RUNNING)
-	    {
-	    	printf("SwiHandler: MMU Switch Process\n");
-	    	MmuSwitchToProcess(proc);
-	    }
+	if (proc != NULL && proc->state == RUNNING) {
+		printf("SwiHandler: MMU Switch Process\n");
+		MmuSwitchToProcess(proc);
+	}
 
 }
